6f80a23c7fff13789b4db15f9b32d2955ae9d8f9
Your Actual Name <el.dyko@gmail.com>
2015-11-19 19:07:27 -0500

    added functionality and a fixed full screen, hiding controls, custom buttons, click anywhere on video to play and pause.

--- videoApp.js
+++ videoApp.js
@@ -4,29 +4,32 @@ var videoApp = {};
 
 videoApp.initPlayer = function() {
 	// Set object references
-	videoApp.vid = document.getElementById("my_video");
-	videoApp.playbtn = document.getElementById("playpausebtn");
-	videoApp.seekslider = document.getElementById("seekslider");
-	videoApp.curtimetext = document.getElementById("curtimetext");
-	videoApp.durtimetext = document.getElementById("durtimetext");
-	videoApp.mutebtn = document.getElementById("mutebtn");
-	videoApp.volumeslider = document.getElementById("volumeslider");
+	videoApp.vid 					= document.getElementById('my_video');
+	videoApp.playbtn 			= document.getElementById('playbtn');
+	videoApp.playIcon 		= document.getElementById('playIcon');
+	videoApp.seekslider 	= document.getElementById('seekslider');
+	videoApp.curtimetext 	= document.getElementById('curtimetext');
+	videoApp.durtimetext 	= document.getElementById('durtimetext');
+	videoApp.mutebtn 			= document.getElementById('mutebtn');
+	videoApp.volumeslider = document.getElementById('volumeslider');
 	// Add event listeners
-	videoApp.playbtn.addEventListener("click",videoApp.playPause,false);
-	videoApp.seekslider.addEventListener("change",videoApp.vidSeek,false);
-	videoApp.vid.addEventListener("timeupdate",videoApp.seektimeupdate,false);
-	videoApp.mutebtn.addEventListener("click",videoApp.vidmute,false);
-	videoApp.volumeslider.addEventListener("change",videoApp.setvolume,false);
+	videoApp.playbtn.addEventListener('click',videoApp.playPause,false);
+	videoApp.seekslider.addEventListener('change',videoApp.vidSeek,false);
+	videoApp.vid.addEventListener('timeupdate',videoApp.seektimeupdate,false);
+	videoApp.mutebtn.addEventListener('click',videoApp.vidmute,false);
+	videoApp.volumeslider.addEventListener('change',videoApp.setvolume,false);
 }
 
-// Play/Pause
+// Play/Pause function and swapping icons
 videoApp.playPause = function() {
 	if(videoApp.vid.paused) {
 		videoApp.vid.play();
-		videoApp.playbtn.innerHTML = "Pause";
+		// $( '#playbtn' ).html( '<i class="fa fa-paused">' );
+		$( '#playPause' ).toggleClass('fa-pause fa-play');
 	} else {
 		videoApp.vid.pause();
-		videoApp.playbtn.innerHTML = "Play";
+		// $( '#playbtn' ).html( '<i class="fa fa-play">' );
+		$( '#playPause' ).toggleClass('fa-play fa-pause');
 	}
 }
 
@@ -44,22 +47,24 @@ videoApp.seektimeupdate = function() {
 	videoApp.cursecs = Math.floor(videoApp.vid.currentTime - videoApp.curmins * 60);
 	videoApp.durmins = Math.floor(videoApp.vid.duration / 60);
 	videoApp.dursecs = Math.floor(videoApp.vid.duration - videoApp.durmins * 60);
-	if(videoApp.cursecs < 10){ videoApp.cursecs = "0"+videoApp.cursecs; }
-	if(videoApp.dursecs < 10){ videoApp.dursecs = "0"+videoApp.dursecs; }
-	if(videoApp.curmins < 10){ videoApp.curmins = "0"+videoApp.curmins; }
-	if(videoApp.durmins < 10){ videoApp.durmins = "0"+videoApp.durmins; }
-	videoApp.curtimetext.innerHTML = videoApp.curmins+":"+videoApp.cursecs;
-	videoApp.durtimetext.innerHTML = videoApp.durmins+":"+videoApp.dursecs;
+	if(videoApp.cursecs < 10){ videoApp.cursecs = '0'+videoApp.cursecs; }
+	if(videoApp.dursecs < 10){ videoApp.dursecs = '0'+videoApp.dursecs; }
+	if(videoApp.curmins < 10){ videoApp.curmins = '0'+videoApp.curmins; }
+	if(videoApp.durmins < 10){ videoApp.durmins = '0'+videoApp.durmins; }
+	videoApp.curtimetext.innerHTML = videoApp.curmins+':'+videoApp.cursecs;
+	videoApp.durtimetext.innerHTML = videoApp.durmins+':'+videoApp.dursecs;
 }
 
 // Mute Button
 videoApp.vidmute = function() {
 	if(videoApp.vid.muted){
 		videoApp.vid.muted = false;
-		videoApp.mutebtn.innerHTML = "Mute";
+		// videoApp.mutebtn.innerHTML = 'Mute';
+		$('#muteIcon').toggleClass('fa-volume-up fa-volume-off');
 	} else {
 		videoApp.vid.muted = true;
-		videoApp.mutebtn.innerHTML = "Unmute";
+		// videoApp.mutebtn.innerHTML = 'Unmute';
+		$('#muteIcon').toggleClass('fa-volume-off fa-volume-up');
 	}
 }
 
@@ -68,8 +73,34 @@ videoApp.setvolume = function() {
 	videoApp.vid.volume = videoApp.volumeslider.value / 100;
 }
 
+// Show / Hide Controls
+// when the div with a class of .controlsToggle gets moused over
+$( '.controlsToggle' ).mouseover(function() {
+	// if the control bar is visible
+	if ( $( '#video_controls_bar' ).is( ':visible' ) ) {
+		// hide the controls bar
+		$( '#video_controls_bar' ).hide( 'slow' );
+		// otherwise show the controls bar
+	} else {
+		$( '#video_controls_bar' ).show( 'slow' );
+	}
+});
+
+// Anywhere on screen play/pause
+$('video').click(function(){
+	videoApp.playPause();
+});
+
+// SpaceBar Press play/pause
+$(window).keypress(function (e) {
+  if (e.keyCode === 0 || e.keyCode === 32) {
+    e.preventDefault()
+    videoApp.playPause();
+  }
+});
+
 // When Page Loads, run this bitch
 $( document ).ready(function() {
-    console.log( "ready!" );
-    videoApp.initPlayer();
+  console.log( 'ready!' );
+  videoApp.initPlayer();
 });
